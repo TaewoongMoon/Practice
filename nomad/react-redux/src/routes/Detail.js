9e37c395ed57f0f4f69c95719d016d83d9f41f3a
TaewoongMoon <taewoongmoon1@gmail.com>
2021-08-19 16:46:27 +0900

    MapStateTo Props practice-2

--- nomad/react-redux/src/routes/Detail.js
+++ nomad/react-redux/src/routes/Detail.js
@@ -1,5 +1,18 @@
-const Detail = () => {
-  return <h1>Detail</h1>;
+import { connect } from "react-redux";
+
+const Detail = ({ toDo }) => {
+  return <h1>{toDo?.text}</h1>;
 };
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  console.log(state);
+  console.log(id);
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
